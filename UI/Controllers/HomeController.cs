af6a5de92bcb2e728523a0d649b7700b83993441
Carlos <cavu93@hotmail.com>
2021-01-16 02:02:46 -0600

    Creacion de capas

---
+++ UI/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void create(string placa, string dueño, string marca)
+        {
+
+            string p, d, m;
+            p = placa;
+            d = dueño;
+            m = marca;
+            //return View();
+            //return Content("hola");
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
